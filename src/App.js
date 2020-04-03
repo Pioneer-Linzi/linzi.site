e2e737f1545ce470c5537db70a1f37d7116a6e53
赵林林 <linlin.zhao@ximalaya.com>
2020-04-04 01:18:57 +0800

    文件

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const arr =[
+    {
+      lable: 'jenkins',
+      url: '//jenkins.linzi.site'
+    },
+    {
+      lable: 'portainer',
+      url: 'http://portainer.linzi.site/'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='list'>
+        {
+          arr.map((item)=>{
+              return   (<a href={item.url}>
+              <div className='item'>
+                <span>
+                    {item.lable}
+                </span>
+              </div>
+              </a>)
+          })
+        }
+      
+      </div>
     </div>
   );
 }
